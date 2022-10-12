324df341df6d0a7cd6fa4d74d1f14a38afb43e11
Dilian Dimitrov <dilian.dimitrov85@gmail.com>
2022-10-12 10:30:50 +0300

    added routing structure and cleaned some unnecessary things from react auto setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header/Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Header, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./Components"; //simple import
 
 function App() {
   return (
-    <Header/>
-);
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" exact element={<VideoDetail />} />
+        <Route path="/channel/:id" exact element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header/Header";
+export { default as Feed } from "./Feed/Feed";
+export { default as VideoDetail } from "./VideoDetail/VideoDetail";
+export { default as ChannelDetail } from "./ChannelDetail/ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed/SearchFeed";
