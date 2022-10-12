610eb4b5a73c6a3a4c896e53bfa5101cb7d06a15
Dilian Dimitrov <dilian.dimitrov85@gmail.com>
2022-10-12 20:52:39 +0300

    removed exact from router paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Header, Feed, VideoDetail, ChannelDetail, SearchFeed, ErrorPage } from "./Components"; //simple import
+import {
+  Header,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+  ErrorPage,
+} from "./Components"; //simple import
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
-        <Route path="/video/:id" exact element={<VideoDetail />} />
-        <Route path="/channel/:id" exact element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
-        <Route path="*" element={<ErrorPage/>}/>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
