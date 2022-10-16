6e6020a0aa1b019a1d17c3330003aecafe6317ab
Dilian Dimitrov <dilian.dimitrov85@gmail.com>
2022-10-16 13:10:24 +0300

    removed videosComponent - not needed, added feed.css and replaced mui elements in feed.jsx with regular html elements

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./feed.css";
 import { useState, useEffect } from "react";
-import { Sidebar, Videos, CategoriesBar } from "..";
+import { Sidebar, CategoriesBar, MainVideoCard, MainChannelCard } from "..";
 
 import fetchFromAPI from "../../utils/fetchFromAPI";
 
@@ -11,22 +11,22 @@ export default function Feed(props) {
   const [selectedCategory, setSelectedCategory] = useState("new"); //първоначално ни зарежда видеа от категория New
   const [videos, setVideos] = useState([]);
 
-  // !За реални резултати от заявка, според категория от таба с категории, разкоментирай това и закоментирай следващия useEffect
-  useEffect(() => {
-    console.log("update");
-    console.log(selectedCategory);
-    fetchFromAPI(
-      `/search?part=snippet&q=${selectedCategory}&maxResults=20`
-    ).then((data) => {
-      console.log(data.items);
-      setVideos(data.items);
-    });
-  }, [selectedCategory]); //когато selectedCategory се промени, изпълни callback ф-ята в useEffect()
-
-  // // //!Разкоментирай това, за да зарежда само demoVideosResponse и да не хаби заявки
+  // // !За реални резултати от заявка, според категория от таба с категории, разкоментирай това и закоментирай следващия useEffect
   // useEffect(() => {
-  //   setVideos(demoVideosResponse);
-  // }, [selectedCategory]); //когато някое от тези двете в масива се промени, изпълни callback ф-ята в useEffect()
+  //   console.log("update");
+  //   console.log(selectedCategory);
+  //   fetchFromAPI(
+  //     `/search?part=snippet&q=${selectedCategory}&maxResults=20`
+  //   ).then((data) => {
+  //     console.log(data.items);
+  //     setVideos(data.items);
+  //   });
+  // }, [selectedCategory]); //когато selectedCategory се промени, изпълни callback ф-ята в useEffect()
+
+  // //!Разкоментирай това, за да зарежда само demoVideosResponse и да не хаби заявки
+  useEffect(() => {
+    setVideos(demoVideosResponse);
+  }, [selectedCategory]); //когато някое от тези двете в масива се промени, изпълни callback ф-ята в useEffect()
 
   return (
     <div className="feedMainContainer">
@@ -41,7 +41,15 @@ export default function Feed(props) {
         />
 
         <div className="resultsContainer">
-          <Videos videos={videos} />
+          {videos.map((vid, idx) => {
+            if (vid.id.videoId) {
+              //значи е видео, а не канал
+              return <MainVideoCard video={vid} key={idx} />;
+            } else {
+              //значи е канал, а не видео
+              return <MainChannelCard channelDetails={vid} key={idx} />;
+            }
+          })}
         </div>
       </div>
     </div>
