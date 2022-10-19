5d121545bfb22741dd96870ccb2855067660d470
Dilian Dimitrov <dilian.dimitrov85@gmail.com>
2022-10-19 17:46:55 +0300

    added autocomplete in search field - needs a little bit more work to work as it should

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,41 +10,40 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useLocation, Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 
 import { SearchBar } from "../";
 import { mahniGo } from "../../store/loggedUser";
 
 export default function Header() {
-    const location = useLocation();
-    
-    let pageWithoutHeader = location.pathname.match(/login|register/gi);
-    const user = useSelector(state => state.loggedUser.user)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-   
-    const handleLoginFromHomePage = (ev) => {
-        //if there's an active user, call the function to log out and update the state to 0 logged users
-        if (user) {
-            dispatch(mahniGo())
-            return
-        }
-        navigate("/login")
+  const location = useLocation();
 
+  let pageWithoutHeader = location.pathname.match(/login|register/gi);
+  const user = useSelector((state) => state.loggedUser.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLoginFromHomePage = (ev) => {
+    //if there's an active user, call the function to log out and update the state to 0 logged users
+    if (user) {
+      dispatch(mahniGo());
+      return;
     }
-    return (
-        <>
-            {!pageWithoutHeader && (
-                <header className="header">
-                    <div className="headerLeft">
-                        <MenuIcon />
-                        <Link to={`/`}>
-                            <img className="ytlogo" src={YoutubeLogo} alt="logo" />
-                        </Link>
-                    </div>
+    navigate("/login");
+  };
+  return (
+    <>
+      {!pageWithoutHeader && (
+        <header className="header">
+          <div className="headerLeft">
+            <MenuIcon />
+            <Link to={`/`}>
+              <img className="ytlogo" src={YoutubeLogo} alt="logo" />
+            </Link>
+          </div>
 
-                    <div className="headerCenter">
-                        {/* <TextField
+          {/* <div className="headerCenter"> */}
+          {/* <TextField
               id="outlined-basic"
               className="searchbar"
               size="small"
@@ -60,17 +59,21 @@ export default function Header() {
                 ),
               }}
             /> */}
-                        <SearchBar />
-                    </div>
-                    <div className="headerRight">
-                        <MoreVertIcon />
-                            <Button onClick={handleLoginFromHomePage} className="userButton" variant="outlined">
-                                <AccountCircleIcon />
-                                {user ? <>Sign out</> : <>Sign in</>}
-                            </Button>
-                    </div>
-                </header>
-            )}
-        </>
-    );
+          <SearchBar />
+          {/* </div> */}
+          <div className="headerRight">
+            <MoreVertIcon />
+            <Button
+              onClick={handleLoginFromHomePage}
+              className="userButton"
+              variant="outlined"
+            >
+              <AccountCircleIcon />
+              {user ? <>Sign out</> : <>Sign in</>}
+            </Button>
+          </div>
+        </header>
+      )}
+    </>
+  );
 }
--- src/Components/searchBar/SearchBar.jsx
+++ src/Components/searchBar/SearchBar.jsx
@@ -1,53 +1,152 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect /*useMemo*/ } from "react";
 import { useNavigate /*redirect*/ } from "react-router-dom"; //да се види redirect вместо useNavigate
-import { Paper, IconButton } from "@mui/material";
+import {
+  /*Paper,*/ IconButton,
+  Autocomplete,
+  TextField,
+  Box,
+} from "@mui/material";
 import { Search } from "@mui/icons-material";
-import "./searchBar.css";
+// import "./searchBar.css";
+
+import fetchFromAPI from "../../utils/fetchFromAPI";
+// import debounce from "lodash.debounce";
 
 export default function SearchBar() {
-  const [searchTerm, setsearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
   const navigate = useNavigate();
 
+  function myDebounce(funcToDebounce, time) {
+    let timerId;
+    return function (...params) {
+      clearTimeout(timerId);
+      timerId = setTimeout(funcToDebounce, time, ...params);
+    };
+  }
+
+  const handleInput = (e) => {
+    if (e.target.value.length > 1) {
+      console.log(e.target.value);
+      setSearchTerm(e.target.value);
+    }
+  };
+
+  useEffect(() => {
+    fetchFromAPI(`/search?part=snippet&q=${searchTerm}&maxResults=10`).then(
+      (suggestedData) => {
+        let suggestedTitles = suggestedData.items.map((el) => el.snippet.title);
+        console.log(suggestedTitles);
+        setSuggestions(suggestedTitles);
+      }
+    );
+  }, [searchTerm]); //когато selectedCategory се промени, изпълни callback ф-ята в useEffect()
+
+  let debounceInput = myDebounce(handleInput, 1000);
+
+  //оn submit
   const handleSubmit = (e) => {
     e.preventDefault(); //защото компонента се води форма и бутона е събмит
 
     if (searchTerm) {
       navigate(`/search/${searchTerm}`); //променяме url-то, за да активираме /search/:searchTerm path, за да ни се зареди searchFeed компонента
-
-      setsearchTerm(""); //накрая зачистваме searchTerm
+      setSearchTerm(""); //накрая зачистваме searchTerm
     }
   };
 
   return (
-    <Paper //Paper is a simple div with white background and some elevation
-      component="form"
+    <Box
+      component={"form"}
       sx={{
-        borderRadius: 0,
-        border: "1px solid #e3e3e3",
-        pl: 1, //padding left shorthand
-        boxShadow: "none",
-        backgroundColor: "#ffffff" /*"#313131"*/,
+        backgroundColor: "#ffffff",
+        display: "flex",
       }}
       onSubmit={handleSubmit}
     >
-      <input
-        className="search-bar"
-        placeholder="Search"
-        value={searchTerm}
-        onInput={(e) => setsearchTerm(e.target.value)}
+      <Autocomplete
+        // value={searchTerm}
+        // open={searchTerm.length > 2}
+        freeSolo={true}
+        onInput={debounceInput}
+        options={suggestions}
+        getOptionLabel={(option) => option}
+        style={{ width: 500, backgroundColor: "#ffffff" }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            sx={{
+              // "& fieldset": { border: "none" },
+              width: 500,
+              boxSizing: "border-box",
+            }}
+          />
+        )}
       />
       <IconButton
         type="submit"
         sx={{
           p: "10px",
           color: "#b1b1b1",
-          backgroundColor: "#f8f8f8",
         }}
       >
         <Search />
       </IconButton>
-    </Paper>
+    </Box>
   );
 }
+
+// import React from "react";
+// import { useState } from "react";
+// import { useNavigate /*redirect*/ } from "react-router-dom"; //да се види redirect вместо useNavigate
+// import { Paper, IconButton } from "@mui/material";
+// import { Search } from "@mui/icons-material";
+// import "./searchBar.css";
+
+// export default function SearchBar() {
+//   const [searchTerm, setsearchTerm] = useState("");
+
+//   const navigate = useNavigate();
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault(); //защото компонента се води форма и бутона е събмит
+
+//     if (searchTerm) {
+//       navigate(`/search/${searchTerm}`); //променяме url-то, за да активираме /search/:searchTerm path, за да ни се зареди searchFeed компонента
+
+//       setsearchTerm(""); //накрая зачистваме searchTerm
+//     }
+//   };
+
+//   return (
+//     <Paper //Paper is a simple div with white background and some elevation
+//       component="form"
+//       sx={{
+//         borderRadius: 0,
+//         border: "1px solid #e3e3e3",
+//         pl: 1, //padding left shorthand
+//         boxShadow: "none",
+//         backgroundColor: "#ffffff" /*"#313131"*/,
+//       }}
+//       onSubmit={handleSubmit}
+//     >
+//       <input
+//         className="search-bar"
+//         placeholder="Search"
+//         value={searchTerm}
+//         onInput={(e) => setsearchTerm(e.target.value)}
+//       />
+//       <IconButton
+//         type="submit"
+//         sx={{
+//           p: "10px",
+//           color: "#b1b1b1",
+//           backgroundColor: "#f8f8f8",
+//         }}
+//       >
+//         <Search />
+//       </IconButton>
+//     </Paper>
+//   );
+// }
