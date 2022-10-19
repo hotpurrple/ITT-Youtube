153cf3f29a6a1c592b8699867f7027aae1c70193
hotpurrple <kiril4ivanov@gmail.com>
2022-10-19 17:09:58 +0300

    updated a comment

--- src/store/loggedUser.js
+++ src/store/loggedUser.js
@@ -6,7 +6,9 @@ const initialState = {
     user: current,
 }
 
-//using functions from the server folder - respectively 
+//using functions from the server folder - logniGo sets the is_logged property of the user that matches the string in the payload,
+// mahniGo sets all the is_logged properties to false so there's no active user. after that the state is set to either
+// whichever object matches the logged property, or to nothing.
 
 export const loggedUser = createSlice({
   name: 'loggedUser',
