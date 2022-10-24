bdf6ea8d990926f5b3a35ecad00a95e76ae313d6
Dilian Dimitrov <dilian.dimitrov85@gmail.com>
2022-10-24 16:03:52 +0300

    updated logic for add to playlist dialog in mainVideoCard

--- src/Components/MainVideoCardDotsBtn/MainVideoCardDotsBtn.jsx
+++ src/Components/MainVideoCardDotsBtn/MainVideoCardDotsBtn.jsx
@@ -6,14 +6,39 @@ import IconButton from "@mui/material/IconButton";
 import * as HiIcons from "react-icons/hi";
 import { useState } from "react";
 
-import { Divider, Button } from "@mui/material";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import { Divider, Button, ListItem, ListItemText } from "@mui/material";
+
 import "./MainVideoCardDotsBtn.css";
 
 export default function MainVideoCardDotsBtn(props) {
-  //тук идва цялото инфо за видеото
   const [dialogShown, setDialogShown] = useState(false);
+  const [showHiddenMenu, setShowHiddenMenu] = useState(false);
+
+  const [newPlaylistName, setNewPlaylistName] = useState("");
+  const handleNewPlaylistNameInput = (e) => {
+    setNewPlaylistName(e.target.value);
+  };
+
+  const handleListItemClick = (playlistName) => {
+    console.log(playlistName);
+  };
+
+  const showTheHiddenMenu = () => {
+    setShowHiddenMenu(!showHiddenMenu);
+  };
+
+  const createNewPlaylist = (e) => {
+    console.log(e);
+    console.log("New playlist name is: " + newPlaylistName);
+  };
+
+  let playlists = [
+    "Playlist 1",
+    "Playlist 2",
+    "Playlist 3",
+    "Playlist 4",
+    "Playlist 5",
+  ];
 
   return (
     <div className={props.className}>
@@ -24,22 +49,43 @@ export default function MainVideoCardDotsBtn(props) {
         <HiIcons.HiDotsHorizontal />
       </IconButton>
 
-      <Dialog
-        // className="modalDialog"
-        onClose={() => setDialogShown(!dialogShown)}
-        open={dialogShown}
-      >
-        <DialogTitle>Add to playlist</DialogTitle>
-        <div className="viewPlaylists">
-          <FormControlLabel control={<Checkbox />} label="Playlist 1" />
-          <FormControlLabel control={<Checkbox />} label="Playlist 2" />
-          <FormControlLabel control={<Checkbox />} label="Playlist 3" />
-        </div>
+      <Dialog onClose={() => setDialogShown(!dialogShown)} open={dialogShown}>
+        <DialogTitle> Add to playlist </DialogTitle>
+        <ul>
+          {playlists.map((pName) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(pName)}
+              key={pName}
+            >
+              <ListItemText primary={pName} />
+            </ListItem>
+          ))}
+        </ul>
+
         <Divider />
 
-        <Button variant="text" color="primary">
+        <Button
+          variant="text"
+          color="primary"
+          onClick={() => showTheHiddenMenu()}
+        >
           Create new playlist
         </Button>
+
+        {showHiddenMenu && (
+          <div className="createNewPlaylistDiv">
+            <input
+              value={newPlaylistName}
+              onInput={handleNewPlaylistNameInput}
+              placeholder="Enter new playlist name"
+              type="text"
+            />
+            <Button variant="text" color="primary" onClick={createNewPlaylist}>
+              Add new playlist
+            </Button>
+          </div>
+        )}
       </Dialog>
     </div>
   );
