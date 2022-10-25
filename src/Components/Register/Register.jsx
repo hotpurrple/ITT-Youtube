1af65617e1f87d4cc12ea8c56afcd2ff5802ae76
hotpurrple <kiril4ivanov@gmail.com>
2022-10-25 03:08:50 +0300

    validated regiister page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import "./register.css";
 import GoogleLogo from "../Login/googlelogo.png";
@@ -8,56 +8,137 @@ import { registerUser } from "../../server/serverRegister";
 import { doesUserExist } from "../../server/serverRegister";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
-
+import debounce from "lodash.debounce"
+import { useState } from "react";
 export default function Register() {
-  const username = useRef();
-  const password = useRef();
-  const navigate = useNavigate()
-  const handleUserRegister = () => {
-    if (doesUserExist(username.current.value)) {
-      alert("You already exist!");
-      return;
-    }
-    registerUser(username.current.value, password.current.value);
-    navigate("/login");
-  };
-  return (
-    <>
-      <div className="regcontainer">
-        <form className="regform" action="">
-          <div className="regmessage">
-            <img src={GoogleLogo} alt="" />
-            <p>Welcome!</p>
-            <p className="regcontinue">Feel free to sign up.</p>
-          </div>
-          <div className="regdetails">
-            <TextField
-              inputRef={username}
-              required
-              InputLabelProps={{ required: false }}
-              label="Username"
-            />
-            <TextField
-              inputRef={password}
-              required
-              InputLabelProps={{ required: false }}
-              label="Password"
-            />
-            <TextField
-              required
-              InputLabelProps={{ required: false }}
-              label="Repeat password"
-            />
+    const username = useRef();
+    const password = useRef();
+    const repeatPassword = useRef()
+    const navigate = useNavigate()
+    const [usernameInvalidReason, setUsernameInvalidReason] = useState("")
+    const [passwordInvalidReason, setPasswordInvalidReason] = useState("")
+    const [matchingPassCheck, setMatchingPassCheck] = useState("")
+    const [userAlreadyExists, setUserAlreadyExists] = useState("")
+    const [disabledButton, setDisabledButton] = useState(true)
+    const [enableButton, setEnableButton] = useState({
+        correctUsername: false,
+        correctPassword: false,
+        passesMatch: false
+    })
 
-            <div className="logHref">
-              <Link to="/login">Have an account?</Link>
-              <Button variant="contained" onClick={handleUserRegister}>
-                Register
-              </Button>
+    const shouldButtonBeEnabled = () => {
+        const shouldButtonBeEnabled = Object.values(enableButton).every(e => e === true)
+        shouldButtonBeEnabled ? setDisabledButton(false) : setDisabledButton(true)
+    }
+    const handleUserRegister = () => {
+        if (doesUserExist(username.current.value)) {
+            setUserAlreadyExists("USER ALREADY EXISTS")
+            return;
+        }
+        registerUser(username.current.value, password.current.value);
+        navigate("/login");
+        
+    };
+
+    useEffect(() => {
+        shouldButtonBeEnabled()   
+    }, [enableButton])
+  
+    const validateRegisterPass = () => {
+        let isPasswordValid =
+            password?.current?.value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
+        if (!isPasswordValid) {
+            setPasswordInvalidReason(`Password is not strong enough. Please make sure it's at least 6 characters long and it
+            contains at least one number and special character.`)
+            setEnableButton({...enableButton, correctPassword: false})
+
+        } else {
+            setPasswordInvalidReason("")
+            setEnableButton({...enableButton, correctPassword: true})
+        }
+    }
+    const validateRegisterName = (usernametest) => {
+        let isUsernameValid = usernametest?.current?.value.match(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/gi)
+        if (!isUsernameValid) {
+            setUsernameInvalidReason(`Invalid username. Please ensure it's 8-20 characters 
+            long and lacks special characters and empty spaces.`)
+            setEnableButton({...enableButton, correctUsername: false})
+        } else {
+            setUsernameInvalidReason("")
+            setEnableButton({...enableButton, correctUsername: true})
+        }
+    }
+  
+
+    const doPassesMatch = (pass, repeat) => {
+        let passwordtest = pass?.current?.value
+        let repeatpassword = repeat?.current?.value
+        let doPassesMatch = passwordtest.toLowerCase().trim() === repeatpassword.toLowerCase().trim()
+        if (!doPassesMatch) {
+            setMatchingPassCheck("Passwords don't match.")
+            setEnableButton({...enableButton, passesMatch: false})
+
+        } else {
+            setMatchingPassCheck("")
+            setEnableButton({...enableButton, passesMatch: true})
+            
+        }
+        
+        
+    }
+    const UpdateRegName = debounce(() => validateRegisterName(username), 1000)
+    const UpdateRegPass = debounce(() => validateRegisterPass(password), 1000)
+    const DoPasswordsMatch = debounce(() => doPassesMatch(password, repeatPassword), 1000)
+    return (
+        <>
+            <div className="regcontainer">
+                <form className="regform" action="">
+                    <div className="regmessage">
+                        <img src={GoogleLogo} alt="" />
+                        <p>Welcome!</p>
+                        <p className="regcontinue">Feel free to sign up.</p>
+                        <Button color="error">{userAlreadyExists}</Button>
+                    </div>
+                    <div className="regdetails">
+                        <TextField
+                            onInput={UpdateRegName}
+                            inputRef={username}
+                            helperText={usernameInvalidReason}
+                            error={usernameInvalidReason}
+                            required
+                            InputLabelProps={{ required: false }}
+                            label="Username"
+                        />
+                        <TextField
+                            inputRef={password}
+                            required
+                            type="password"
+                            onInput={UpdateRegPass}
+                            error={passwordInvalidReason}
+                            helperText={passwordInvalidReason}
+                            InputLabelProps={{ required: false }}
+                            label="Password"
+                        />
+                        <TextField
+                            required
+                            inputRef={repeatPassword}
+                            type="password"
+                            onInput={DoPasswordsMatch}
+                            InputLabelProps={{ required: false }}
+                            helperText={matchingPassCheck}
+                            error={matchingPassCheck}
+                            label="Repeat password"
+                        />
+
+                        <div className="logHref">
+                            <Link to="/login">Have an account?</Link>
+                            <Button variant="contained" disabled={disabledButton} onClick={handleUserRegister}>
+                                Register
+                            </Button>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+        </>
+    );
 }
