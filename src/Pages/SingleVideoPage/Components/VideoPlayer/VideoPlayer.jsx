98019ec82cafc66219074f18b4bc90c138e23d1a
hotpurrple <kiril4ivanov@gmail.com>
2022-11-01 19:12:41 +0200

    reworked fetch function a little bit

--- src/Pages/SingleVideoPage/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Pages/SingleVideoPage/Components/VideoPlayer/VideoPlayer.jsx
@@ -4,20 +4,14 @@ import { useEffect } from 'react'
 
 export default function VideoPlayer(props) {
     const url = `http://www.youtube.com/embed/${props.link}`
-    const [element, setElement] = useState()
-
-    useEffect(() => {
-        setTimeout(() => {
-
-        }, 6000)
-    })
 
     return (
         <>
-            <iframe className='ytIframe' allowtransparency="true"
-                title={url} crossOrigin="anonymous" width="100%" height="100%"
+            {/* https://www.youtube.com/watch?v=-HXF6GCAPuw */}
+            <iframe className='ytIframe' allowtransparency="true" 
+            title={url} crossOrigin="anonymous" width="100%" height="100%"
                 src={url}>
-            </iframe>  :
+            </iframe> 
 
         </>
     )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,7 @@
 const keys = [
     "291c8adc40msh2e2fa55f0195de1p13c8c0jsn6e2392beeef29",
     "4810b08f52msh8dc3e3709d6b9aap174b4ajsn1bab90c421fa",
+    "d94c2a7f54msh2fffefec5010dcep126f6ajsn2c4ad0a6316b",
     "33506328d3msh98631daf5531085p1fbbf1jsn0f525a5e83a3",
     "9d8dda8991mshcabe783f2d52885p1ed4e1jsn7ad52d926009",
     "09970a171cmsh8b55fc94efac597p1ef987jsne768ecca4f1b",
@@ -37,25 +38,39 @@ const BASE_URL = `https://youtube-v31.p.rapidapi.com`;
 
 
 export default function fetchFromApi(uri) {
-    console.log(Promise.any(keys.map(e => fetcher(uri, e))));
-    return Promise.any(keys.map(e => fetcher(uri, e)))
-        .then(response => {
-            return response.json()
-        }
-    )
+    // console.log(Promise.any(keys.map(e => fetcher(uri, e))));
+    // return Promise.any(keys.map(e => fetcher(uri, e)))
+    //     .then(response => {
+    //         return response.json()
+    //     }
+    // )
+    return fetcher(uri, 8)
 }
 
 
 
-function fetcher(uri, apiKey) {
+function fetcher(uri, retries=8) {
 
-    options.headers["X-RapidAPI-Key"] = apiKey
     //приема динамичен uri, който се закача на BASE_URL и ползва горните options
-    return fetch(BASE_URL + uri, options).then((res) => {
-        if (!res.ok) {
-            //ако статуса е в неуспешния диапазон - 4xx - 5xx, хвърли тази грешка, за да може после да се влезе в catch
-            throw new Error(`Error: ${res.status}. Message: ${res.message}`);
-        }
-        return res;
-    });
+    // return fetch(BASE_URL + uri, options).then((res) => {
+    //     if (!res.ok) {
+    //         //ако статуса е в неуспешния диапазон - 4xx - 5xx, хвърли тази грешка, за да може после да се влезе в catch
+    //         throw new Error(`Error: ${res.status}. Message: ${res.message}`);
+    //     }
+    //     return res;
+    // });
+
+   options.headers["X-RapidAPI-Key"] = keys[retries]
+   return fetch(BASE_URL + uri, options)
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      if (retries > 0) {
+        options.headers["X-RapidAPI-Key"] = keys[retries -1]
+        return fetcher(uri, retries - 1)
+      }
+      throw new Error(res.status)
+    })
+    .catch(error => console.error(error.message))
 }
